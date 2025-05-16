d725d74dfeb6f70abb599f56450a4b210a9a0190
Isabela <147876367+beazzuz@users.noreply.github.com>
2025-05-16 17:04:46 -0300

    #frontend -telas home e autenticação

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Cadastro from './pages/Cadastro';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+    </Routes>
+  );
+}
+
+export default App;
