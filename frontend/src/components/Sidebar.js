30dfb85b66403e0807d636ae4356329bbb1082f2
nicocarreiro <nicolascarreiror@gmail.com>
2025-05-28 00:01:38 -0300

    fix: menu instead of text

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,15 +1,17 @@
 import * as React from 'react';
-import { useNavigate } from "react-router-dom"; 
-
 import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
 import CssBaseline from '@mui/material/CssBaseline';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import MenuIcon from '@mui/icons-material/Menu';
-import AssessmentIcon from '@mui/icons-material/Assessment';
+import InsertChartIcon from '@mui/icons-material/InsertChart';
 
 const drawerWidth = 240;
 const closedWidth = 60;
@@ -46,7 +48,6 @@ const StyledDrawer = styled(Drawer, {
       ...openedMixin(theme),
       position: 'relative',
       height: '100%',
-      whiteSpace: 'normal',
     },
   }),
   ...(!open && {
@@ -61,7 +62,6 @@ const StyledDrawer = styled(Drawer, {
 
 export default function Sidebar({ open, setOpen }) {
   const theme = useTheme();
-  const navigate = useNavigate();
 
   const toggleDrawer = () => {
     setOpen(!open);
@@ -85,34 +85,30 @@ export default function Sidebar({ open, setOpen }) {
           </IconButton>
         </Box>
 
-        {open ? (
-          <Box sx={{ 
-            p: 2,
-            wordWrap: 'break-word',
-            overflowWrap: 'break-word',
-          }}>
-            <Typography variant="h6" sx={{ mb: 2, fontWeight: 'bold' }}>
-              Sidebar
-            </Typography>
-            <Typography variant="body1" sx={{ whiteSpace: 'normal', mb: 2 }}>
-              WelLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Typography>
-            <Typography variant="body2" sx={{ whiteSpace: 'normal' }}>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.
-            </Typography>
-          </Box>
-        ) : (
-          <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            pt: 2,
-            gap: 3
-          }}>
-            <AssessmentIcon fontSize="medium" />
-          </Box>
-        )}
+        <List>
+          <ListItem disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                <InsertChartIcon />
+              </ListItemIcon>
+              <ListItemText primary="statistics" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+        </List>
       </StyledDrawer>
     </Box>
   );
 }
+
